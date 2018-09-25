50a560f92bf18451cfe31a02ee70bab8f96a4920
danovity <sean.xiaoxu.ye@gmail.com>
2018-09-24 21:53:19 -0400

    finished the layout when camera button is pressed

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,25 +1,10 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class App extends Component {
-  state = { users: [] };
-
-  componentDidMount() {
-    fetch("/users")
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
+  state = {};
 
   render() {
-    return (
-      <div className="App">
-        {this.state.users.map(user => (
-          <div key={user.id}>
-            user: {user.name} Password: {user.password}
-          </div>
-        ))}
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
