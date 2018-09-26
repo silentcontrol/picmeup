f1059d34997ef58d33ff3630b9006c897cfd0f5a
danovity <sean.xiaoxu.ye@gmail.com>
2018-09-26 16:38:03 -0400

    trialed the remote database

--- bin/server_up.js
+++ bin/server_up.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
-var shell = require('shelljs');
+var shell = require("shelljs");
 
-shell.echo('Running shell commands...')
-shell.exec('export GOOGLE_APPLICATION_CREDENTIALS=MyFirstProject-f27240d3a3f3.json && nodemon ./bin/www');
+shell.echo("Running shell commands...");
+shell.exec(
+  "export GOOGLE_APPLICATION_CREDENTIALS=MyFirstProject-f27240d3a3f3.json && nodemon ./bin/www"
+);
--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,71 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var ENV = 'development';
-var knexConfig = require('../knexfile.js');
-var knex = require('knex')(knexConfig[ENV]);
-var dbQuery = require('../db/queryHelper')(knex);
-var dbInsert = require('../db/insertHelper')(knex);
+var ENV = "development";
+var knexConfig = require("../knexfile.js");
+var knex = require("knex")(knexConfig[ENV]);
+var dbQuery = require("../db/queryHelper")(knex);
+var dbInsert = require("../db/insertHelper")(knex);
 
-var express = require('express');
+var express = require("express");
 const router = express.Router();
-var multer = require('multer');
-const { extractWebAnnotations, multerConfig } = require('./helpers.js');
+var multer = require("multer");
+const { extractWebAnnotations, multerConfig } = require("./helpers.js");
 const upload = multer(multerConfig);
 
-const vision = require('@google-cloud/vision');
+const vision = require("@google-cloud/vision");
 const client = new vision.ImageAnnotatorClient();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.redirect('/products');
+router.get("/", (req, res) => {
+  res.redirect("/products");
 });
 
 /* GET list of products */
-router.get('/products', async (req, res) => {
-  await dbInsert.addProduct('hey you', 1000);
-  res.json([{
-    text: 'GET /products routing'
-  }]);
+router.get("/products", async (req, res) => {
+  await dbInsert.addProduct("hi.", 1000);
+  res.json([
+    {
+      text: "GET /products routing"
+    }
+  ]);
 });
 
 /* GET results for search query stirng */
-router.post('/search', (req, res) => {
-  res.json([{
-    text: 'GET /search routing'
-  }]);
+router.post("/search", (req, res) => {
+  res.json([
+    {
+      text: "GET /search routing"
+    }
+  ]);
 });
 
 /* POST order to server */
-router.post('/orders', (req, res) => {
+router.post("/orders", (req, res) => {
   res.sendStatus(200);
 });
 
 /* POST image to server for google vision api annotations */
-router.post('/annotations', upload.single('image'), (req, res) => {
+router.post("/annotations", upload.single("image"), (req, res) => {
   const imageBuffer = req.file.buffer;
-  Promise.all([client.webDetection({
-    image: {
-      'content': imageBuffer
-    }
-  })])
+  Promise.all([
+    client.webDetection({
+      image: {
+        content: imageBuffer
+      }
+    })
+  ])
     .then(results => {
-      res.json([{
-        text: 'POST /annotations',
-        webAnnotations: extractWebAnnotations(results[0])
-      }]);
+      res.json([
+        {
+          text: "POST /annotations",
+          webAnnotations: extractWebAnnotations(results[0])
+        }
+      ]);
     })
     .catch(err => {
-      console.error('Error:', err);
+      console.error("Error:", err);
       res.sendStatus(500);
-    })
+    });
 });
 
 module.exports = router;
